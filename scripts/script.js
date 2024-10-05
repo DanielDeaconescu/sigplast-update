187efc200d443a62f1ba6d82ada0d19bf150a141
danieldeaconescu <daniel.deaconescu23@gmail.com>
2024-10-05 19:19:07 +0300

    further adjustments

---
+++ scripts/script.js
@@ -0,0 +1,54 @@
+function isElementInViewport(el) {
+  if (typeof jQuery === "function" && el instanceof jQuery) {
+    el = el[0];
+  }
+
+  const rect = el.getBoundingClientRect();
+  return (
+    (rect.top <= 0 && rect.bottom >= 0) ||
+    (rect.bottom >=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.top <=
+        (window.innerHeight || document.documentElement.clientHeight)) ||
+    (rect.top >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight))
+  );
+}
+
+const scroll =
+  window.requestAnimation ||
+  function (callback) {
+    window.setTimeout(callback, 1000 / 60);
+  };
+
+const elementsToShow = document.querySelectorAll(".show-on-scroll");
+
+function loop() {
+  elementsToShow.forEach(function (element) {
+    if (isElementInViewport(element)) {
+      element.classList.add("is-visible");
+    } else {
+      element.classList.remove("is-visible");
+    }
+  });
+  scroll(loop);
+}
+
+loop();
+
+// Cookie Section
+
+const cookieContainer = document.querySelector(".cookie-container");
+const cookieButton = document.querySelector(".cookie-btn");
+
+cookieButton.addEventListener("click", () => {
+  cookieContainer.classList.remove("active");
+  localStorage.setItem("cookieBannerDisplayed", "true");
+});
+
+setTimeout(() => {
+  if (!localStorage.getItem("cookieBannerDisplayed")) {
+    cookieContainer.classList.add("active");
+  }
+}, 2000);
