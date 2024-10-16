b7453fde3b928ea517b75bc458c0595a19fbc54a
danieldeaconescu <daniel.deaconescu23@gmail.com>
2024-10-16 03:22:28 +0300

    further changes

---
+++ form_process_factory.php
@@ -0,0 +1,73 @@
+<?php 
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $fullName = filter_input(INPUT_POST, 'full-name', FILTER_SANITIZE_STRING);
+    $phoneNum = filter_input(INPUT_POST, 'phone-num', FILTER_SANITIZE_STRING);
+    $location = filter_input(INPUT_POST, 'location', FILTER_SANITIZE_STRING);
+    $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_STRING);
+    $orderDescription = filter_input(INPUT_POST, 'order-description', FILTER_SANITIZE_STRING);
+
+    if(strlen($fullName) > 100 || strlen($phoneNum) > 100 || strlen($location) > 100 || strlen($email) > 100){
+        die("Too large input data.");
+    }
+
+    // full-name and phone number required
+    if(empty($fullName) || empty($phoneNum) || strlen($phoneNum) < 7){
+        die("Numele si un numar de telefon valid sunt obligatorii.");
+    }
+
+    try {
+        $mail->isSMTP();                                            
+        $mail->Host       = 'mail.danieldeaconescu.com';
+        $mail->SMTPAuth   = true;
+        $mail->Username   = 'contact@danieldeaconescu.com';
+        $mail->Password   = 'contactPassword!23';
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+        $mail->Port       = 465; 
+    
+        //Recipients
+        $mail->setFrom('contact@danieldeaconescu.com', "Sigplast website");
+        $mail->addAddress('daniel.deaconescu98@gmail.com', "$name");
+        $mail->CharSet = 'UTF-8';
+
+        //Content
+        $mail->isHTML(true);
+        $mail->Subject = 'Formular Fabrică ' . $fullName;
+        $mail->Body    = "
+            <h3>Informatii Comanda Fabrică</h3>
+            <div>
+                <p>Nume: <strong>$fullName</strong></p>
+            </div>
+            <div>
+                <p>Numar de telefon: <strong>$phoneNum</strong></p>
+            </div>
+            <div>
+                <p>Localitate: <strong>$location</strong></p>
+            </div>
+            <div>
+                <p>Email: <strong>$email</strong></p>
+            </div>
+            <div>
+                <p>Descrierea lucrarii:        <strong>$orderDescription</strong></p>
+            </div>
+            
+        ";
+        // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+    
+        $mail->send();
+        header('Location: submitted.html');
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+}
--- scripts/script.js
+++ scripts/script.js
@@ -2,13 +2,23 @@
 
 const navigation = document.querySelector(".navbar");
 const formContainer = document.querySelector(".form-container");
-const workshopForm = document.querySelector(".workshop_form");
-const cancelFormBtn = document.querySelector(".btn-cancel-custom");
+const cancelFormBtn = document.querySelectorAll(".btn-cancel-custom");
 const overlay = document.querySelector(".overlay");
 const formFactoryBtn = document.querySelector(".factory-workshop__factory");
 const formWorkshopBtn = document.querySelector(".factory-workshop__workshop");
 const initialDisplay = document.querySelector(".initial-display");
+const workshopForm = document.querySelector(".workshop-form");
 const factoryForm = document.querySelector(".factory-form");
+
+const workshopFormInner = document.querySelector(".workshop_form");
+const factoryFormInner = document.querySelector(".factory_form");
+
+const formInitialButtonsContainer = document.querySelector(
+  ".form-initial-buttons-container"
+);
+const closeFormButton = document.querySelectorAll(".btn-close");
+const formButton = document.querySelector(".side-buttons-form-button");
+const sideButtons = document.querySelector(".side-buttons");
 
 function isElementInViewport(el) {
   if (typeof jQuery === "function" && el instanceof jQuery) {
@@ -77,13 +87,39 @@ const closeForm = function () {
   overlay.classList.add("no-display");
 };
 
-const resetForm = function () {
-  workshopForm.reset();
+// function that closes a form based on a value
+const closeFormParticular = function (formType) {
+  if (formType === "factory") factoryForm.classList.add("d-none");
+  if (formType === "workshop") workshopForm.classList.add("d-none");
 };
 
-cancelFormBtn.addEventListener("click", resetForm);
+const resetForm = function () {
+  workshopFormInner.reset();
+  factoryFormInner.reset();
+  // make sure both forms are closed
+  closeFormParticular("factory");
+  closeFormParticular("workshop");
 
-overlay.addEventListener("click", closeForm);
+  // display inital screen of the form
+  initialDisplay.classList.remove("d-none");
+};
+
+cancelFormBtn.forEach((cancelButton) =>
+  cancelButton.addEventListener("click", resetForm)
+);
+
+// closeFormButton.addEventListener("click", resetForm);
+closeFormButton.forEach((closeButton) => {
+  closeButton.addEventListener("click", resetForm);
+});
+
+document.addEventListener("click", function (e) {
+  if (e.target.classList.contains("modal")) resetForm();
+});
+
+// overlay.addEventListener("click", function (e) {
+//   console.log(e);
+// });
 document.addEventListener("keydown", function (e) {
   if (e.key === "Escape") closeForm();
 });
@@ -94,6 +130,7 @@ workshopForm.addEventListener("submit", function () {
   }, 100);
 });
 
+// close the navigation when clicking outside of it
 document.addEventListener("click", function (e) {
   if (!navigation.contains(e.target)) {
     document.querySelector("#navbarNav").classList.remove("show");
@@ -101,12 +138,60 @@ document.addEventListener("click", function (e) {
       .querySelector(".navbar-toggler")
       .setAttribute("aria-expanded", "false");
     document.querySelector(".navbar-toggler").classList.add("collapsed");
-  } else {
-    console.log("outside the navigation");
   }
 });
 
-formFactoryBtn.addEventListener("click", function () {
-  initialDisplay.classList.add("d-none");
-  factoryForm.classList.remove("d-none");
+// formFactoryBtn.addEventListener("click", function () {
+//   initialDisplay.classList.add("d-none");
+//   factoryForm.classList.remove("d-none");
+// });
+
+// formWorkshopBtn.addEventListener("click", function () {
+//   initialDisplay.classList.add("d-none");
+//   workshopForm.classList.remove("d-none");
+// });
+
+// function that opens a form based on a given input
+const openForm = function (formType) {
+  if (formType === "factory") {
+    // make sure the workshop form is closed
+    workshopForm.classList.add("d-none");
+    // display the factory form
+    initialDisplay.classList.add("d-none");
+    factoryForm.classList.remove("d-none");
+  }
+  if (formType === "workshop") {
+    // make sure the factory form is closed
+    factoryForm.classList.add("d-none");
+    // display the workshop form
+    initialDisplay.classList.add("d-none");
+    workshopForm.classList.remove("d-none");
+  }
+};
+
+// Using event delegation in order to make the form work as expected
+formInitialButtonsContainer.addEventListener("click", function (e) {
+  const clickedElement = e.target.closest(".form-button-initial");
+  if (clickedElement.classList.contains("factory-workshop__factory")) {
+    openForm("factory");
+  }
+  if (clickedElement.classList.contains("factory-workshop__workshop")) {
+    openForm("workshop");
+  }
+});
+
+// formButton hover effect
+formButton.addEventListener("mouseover", function (e) {
+  const hovered = e.target.closest(".side-buttons-form-button");
+  if (hovered) {
+    document
+      .querySelector(".message-form-inner")
+      .classList.remove("message-form-inner-hidden");
+  }
+});
+
+formButton.addEventListener("mouseout", function (e) {
+  document
+    .querySelector(".message-form-inner")
+    .classList.add("message-form-inner-hidden");
 });
